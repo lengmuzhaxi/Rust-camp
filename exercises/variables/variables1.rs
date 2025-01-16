b4bc21813276b6cb9fd21202b7b54a717641a769
lengmuzhaxi <2690497440@qq.com>
2025-01-16 22:13:59 +0800

    variables2.rs

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -4,9 +4,6 @@
 //
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
 fn main() {
     let x:i8 = 5;
     println!("x has the value {}", x);
